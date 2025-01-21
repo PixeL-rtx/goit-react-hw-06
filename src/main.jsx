c58564fc8e483dfb959492b3f0df354c597d86c3
PixeL-rtx <zakroievdenis@icloud.com>
2025-01-21 13:07:08 +0100

    goit-react-hw-06_3

--- src/main.jsx
+++ src/main.jsx
@@ -12,8 +12,9 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}></PersistGate>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
